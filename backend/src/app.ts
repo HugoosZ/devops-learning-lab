65c2d124ffb0fd3e4a9ef122f2aa6819208ec303
HugoosZ <132111633+HugoosZ@users.noreply.github.com>
2026-04-01 16:55:41 -0300

    fix: 404 a la hora de hacer login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3001;
 const HOST = '0.0.0.0';
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: false }));
+app.use(cors({ origin: '*', credentials: false }));
 app.use(bodyParser.json());
 
 setAuthRoutes(app);
