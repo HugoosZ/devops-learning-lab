22ccf9087916984cc2b7d874e889bf13537323b5
HugoosZ <132111633+HugoosZ@users.noreply.github.com>
2026-04-01 16:48:45 -0300

    chore: conexion entre front y backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,12 +5,13 @@ import { setAuthRoutes } from './routes/auth.routes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const HOST = '0.0.0.0';
 
 app.use(cors({ origin: 'http://localhost:3000', credentials: false }));
 app.use(bodyParser.json());
 
 setAuthRoutes(app);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
